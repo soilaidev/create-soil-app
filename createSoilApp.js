1adffcf408cae0714596b28b82fbdb45e5eafb2f
Daniel Murawsky <d.r.murawsky@gmail.com>
2024-08-16 21:33:24 -0400

    chore: Update .gitignore and package.json

--- createSoilApp.js
+++ createSoilApp.js
@@ -1,5 +1,8 @@
+// @ts-check
 "use strict";
 const commander = require("commander");
+const envinfo = require("envinfo");
+const chalk = require("chalk");
 const packageJson = require("./package.json");
 
 let projectName;
@@ -49,141 +52,6 @@ function init() {
     console.log(`Run ${chalk.cyan(`${program.name()} --help`)} to see all options.`);
     process.exit(1);
   }
-
-  function getInstallPackage(version, originalDirectory) {
-    let packageToInstall = "soilai";
-    const validSemver = semver.valid(version);
-    if (validSemver) {
-      packageToInstall += `@${validSemver}`;
-    } else if (version) {
-      if (version[0] === "@" && !version.includes("/")) {
-        packageToInstall += version;
-      } else if (version.match(/^file:/)) {
-        packageToInstall = `file:${path.resolve(originalDirectory, version.match(/^file:(.*)?$/)[1])}`;
-      } else {
-        // for tar.gz or alternative paths
-        packageToInstall = version;
-      }
-    }
-
-    return Promise.resolve(packageToInstall);
-  }
-}
-
-function checkAppName(appName) {
-  const validationResult = validateProjectName(appName);
-  if (!validationResult.validForNewPackages) {
-    console.error(
-      chalk.red(`Cannot create a project named ${chalk.green(`"${appName}"`)} because of npm naming restrictions:\n`)
-    );
-    [...(validationResult.errors || []), ...(validationResult.warnings || [])].forEach((error) => {
-      console.error(chalk.red(`  * ${error}`));
-    });
-    console.error(chalk.red("\nPlease choose a different project name."));
-    process.exit(1);
-  }
-
-  // TODO: there should be a single place that holds the dependencies
-  const dependencies = ["react", "react-dom", "react-scripts"].sort();
-  if (dependencies.includes(appName)) {
-    console.error(
-      chalk.red(
-        `Cannot create a project named ${chalk.green(
-          `"${appName}"`
-        )} because a dependency with the same name exists.\n` +
-          `Due to the way npm works, the following names are not allowed:\n\n`
-      ) +
-        chalk.cyan(dependencies.map((depName) => `  ${depName}`).join("\n")) +
-        chalk.red("\n\nPlease choose a different project name.")
-    );
-    process.exit(1);
-  }
-}
-
-function createApp(name, verbose, version, template, useYarn, usePnp) {
-  const unsupportedNodeVersion = !semver.satisfies(
-    // Coerce strings with metadata (i.e. `15.0.0-nightly`).
-    semver.coerce(process.version),
-    ">=14"
-  );
-
-  if (unsupportedNodeVersion) {
-    console.log(
-      chalk.yellow(
-        `You are using Node ${process.version} so the project will be bootstrapped with an old unsupported version of tools.\n\n` +
-          `Please update to Node 14 or higher for a better, fully supported experience.\n`
-      )
-    );
-    // Fall back to latest supported react-scripts on Node 4
-    version = "react-scripts@0.9.x";
-  }
-
-  const root = path.resolve(name);
-  const appName = path.basename(root);
-
-  checkAppName(appName);
-  fs.ensureDirSync(name);
-  if (!isSafeToCreateProjectIn(root, name)) {
-    process.exit(1);
-  }
-  console.log();
-
-  console.log(`Creating a new React app in ${chalk.green(root)}.`);
-  console.log();
-
-  const packageJson = {
-    name: appName,
-    version: "0.1.0",
-    private: true,
-  };
-  fs.writeFileSync(path.join(root, "package.json"), JSON.stringify(packageJson, null, 2) + os.EOL);
-
-  const originalDirectory = process.cwd();
-  process.chdir(root);
-  if (!useYarn && !checkThatNpmCanReadCwd()) {
-    process.exit(1);
-  }
-
-  if (!useYarn) {
-    const npmInfo = checkNpmVersion();
-    if (!npmInfo.hasMinNpm) {
-      if (npmInfo.npmVersion) {
-        console.log(
-          chalk.yellow(
-            `You are using npm ${npmInfo.npmVersion} so the project will be bootstrapped with an old unsupported version of tools.\n\n` +
-              `Please update to npm 6 or higher for a better, fully supported experience.\n`
-          )
-        );
-      }
-      // Fall back to latest supported react-scripts for npm 3
-      version = "react-scripts@0.9.x";
-    }
-  } else if (usePnp) {
-    const yarnInfo = checkYarnVersion();
-    if (yarnInfo.yarnVersion) {
-      if (!yarnInfo.hasMinYarnPnp) {
-        console.log(
-          chalk.yellow(
-            `You are using Yarn ${yarnInfo.yarnVersion} together with the --use-pnp flag, but Plug'n'Play is only supported starting from the 1.12 release.\n\n` +
-              `Please update to Yarn 1.12 or higher for a better, fully supported experience.\n`
-          )
-        );
-        // 1.11 had an issue with webpack-dev-middleware, so better not use PnP with it (never reached stable, but still)
-        usePnp = false;
-      }
-      if (!yarnInfo.hasMaxYarnPnp) {
-        console.log(
-          chalk.yellow(
-            "The --use-pnp flag is no longer necessary with yarn 2 and will be deprecated and removed in a future release.\n"
-          )
-        );
-        // 2 supports PnP by default and breaks when trying to use the flag
-        usePnp = false;
-      }
-    }
-  }
-
-  run(root, appName, version, verbose, originalDirectory, template, useYarn, usePnp);
 }
 
 module.exports = {
